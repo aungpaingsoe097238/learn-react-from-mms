2cbe389ba3ba344514743187bcf05e7daf49c871
AungPaingSoe <aungpaingsoe097238@gmail.com>
2023-01-02 11:44:38 +0630

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
-import Contacts from "./components/Contacts"
-import Create from "./components/Create"
-import Edit from "./components/Edit"
 
 function App() {
   return (
     <div className=" container mx-auto">
       <Routes>
-        <Route path="/" element={<Contacts />} />
-        <Route path="/create" element={<Create/>}/>
-        <Route path="/edit/:id" element={<Edit/>}/>
       </Routes>
     </div>
   );
